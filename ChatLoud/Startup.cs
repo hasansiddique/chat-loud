77a6fd21c789671143886d4fd9afa53d0389c651
hasanSiddique <hasan.chauhan91@gmail.com>
2016-03-31 23:40:15 +0500

    [First commit] - Initial setup with WebAPI project and a client project

---
+++ ChatLoud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatLoud.Startup))]
+namespace ChatLoud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
